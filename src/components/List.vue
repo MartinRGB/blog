c4f67474c9bda84e873615458c01bf1edccc4c37
MartinRGB <qiuyinsen@gmail.com>
2017-09-20 19:08:15 +0800

    update for backup
    update for backup

--- src/components/List.vue
+++ src/components/List.vue
@@ -9,7 +9,6 @@
           
           <router-link to="/test"><li><a >2.Three Test</a></li></router-link>
           <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-          <li><a v-bind:href="rawHTML">Docs for This Template</a></li>
         </ul>
       </div>
     </div>
@@ -18,11 +17,9 @@
 
 <script>
 
-import htmlFile2 from './../static/articles/test3.html';
 export default {
   name: 'list',
     data: function () {return {
-      rawHTML:htmlFile2,
     }},
   methods: {
   },
