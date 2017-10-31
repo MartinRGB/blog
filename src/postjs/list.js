20249e1eaccc783081a6c31816ef47f5b3b9396c
MartinRGB <qiuyinsen@gmail.com>
2017-10-31 14:28:01 +0800

    ignore design-asset
    ignore design-asset

--- src/postjs/list.js
+++ src/postjs/list.js
@@ -4,10 +4,4 @@ import Article from '../posts/Article.vue'
 Vue.component('Article', Article)
 import ThreeTest from '../posts/ThreeTest.vue'
 Vue.component('ThreeTest', ThreeTest)
-import ThreeTest2 from '../posts/ThreeTest2.vue'
-Vue.component('ThreeTest2', ThreeTest2)
-import ThreeTest3 from '../posts/ThreeTest3.vue'
-Vue.component('ThreeTest3', ThreeTest3)
-import ThreeTest4 from '../posts/ThreeTest4.vue'
-Vue.component('ThreeTest4', ThreeTest4)
-export default [Article, ThreeTest, ThreeTest2, ThreeTest3, ThreeTest4]
+export default [Article, ThreeTest]
--- src/postjs/list.vue
+++ src/postjs/list.vue
@@ -1,6 +1,6 @@
 <template>
         <ul>
 <!-- Add List Here -->  
-<router-link to=Article><li><a>1.砖块着色器</a></li></router-link> <router-link to=ThreeTest3><li><a>2.ThreeTest</a></li></router-link> <router-link to=ThreeTest4><li><a>4.ThreeTest</a></li></router-link> <router-link to=ThreeTest2><li><a>99.ThreeTest</a></li></router-link> <router-link to=ThreeTest><li><a>101.ThreeTest</a></li></router-link>  
+<router-link to=Article><li><a>1.砖块着色器</a></li></router-link> <router-link to=ThreeTest><li><a>101.ThreeTest</a></li></router-link>  
         </ul>
 </template>
