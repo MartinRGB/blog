0a53d67a7dbf4ed9de4c797156627f94a57f4c36
MartinRGB <qiuyinsen@gmail.com>
2020-09-21 17:15:08 +0800

    *update
    *update

--- src/postjs/list.vue
+++
@@ -1,7 +0,0 @@
-
-<template>
-  <ul>
-    <!-- Add List Here -->
-<router-link to=Guide><li><a>1.使用指南</a></li></router-link><router-link to=Brick><li><a>2.[总结]砖块着色器</a></li></router-link><router-link to=Vulkan01><li><a>3.[译] Vulkan 入门系列 —— 综述</a></li></router-link><router-link to=Vulkan02><li><a>4.[译] Vulkan 入门系列 —— 开发环境</a></li></router-link><router-link to=Vulkan03><li><a>5.[译] Vulkan 入门系列 —— 绘制三角形 | 设置 | 基础代码测试</a></li></router-link><router-link to=Vulkan04><li><a>6.[译] Vulkan 入门系列 —— 绘制三角形 | 设置 | 实例化</a></li></router-link><router-link to=Vulkan05><li><a>7.[译] Vulkan 入门系列 —— 绘制三角形 | 设置 | 验证层</a></li></router-link><router-link to=RK4><li><a>8.[译]可视化阐述龙格库塔</a></li></router-link><router-link to=FBM><li><a>9.[总结]FBM 实践与优化心得 (上） —— WebGL 篇</a></li></router-link><router-link to=Sketch_Round_Corner><li><a>10.[总结]Sketch 平滑圆角在 Android 和 Web 中的绘制</a></li></router-link><router-link to=Figma_Round_Corner><li><a>11.[译 + 总结]不顾一切找圆角 —— Figma 实时平滑圆角方案探究</a></li></router-link><router-link to=Android_Overscroller><li><a>12.[总结]Android Overscroller 动画原理分析</a></li></router-link><router-link to=Android_Decay_Animation><li><a>13.[总结]Android 衰减类动画原理分析</a></li></router-link> 
-  </ul>
-</template>
