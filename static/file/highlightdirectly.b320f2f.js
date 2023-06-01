fb31598205fcd781684d7036e86b6676a9e82bd3
MartinRGB <qiuyinsen@gmail.com>
2023-06-01 05:51:51 -0700

    update
    update

--- static/file/highlightdirectly.b320f2f.js
+++
@@ -1,7 +0,0 @@
-highlight:function(){
-    var aCodes = this.$el.getElementsByTagName('pre');
-    for (var i=0; i < aCodes.length; i++) {
-        hljs.highlightBlock(aCodes[i]);
-        hljs.lineNumbersBlock(aCodes[i]);
-    }
-}
--- static/file/waveGraph1.0579028.frag
+++
@@ -1,3 +0,0 @@
-float amplitude = 1.;
-float frequency = 1.;
-y = amplitude * sin(x * frequency);
